7f6fdf15620aaf835d0c540316ff2585fd7e7ad7
5y0ung4 <qwerrr504@naver.com>
2025-06-13 17:56:10 +0900

    저장 기능 미완성 상태입니다

--- backend/interpretation/urls.py
+++ backend/interpretation/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'interpretation'
 urlpatterns = [
     path('interpretation/<str:content>' , views.interpretation_view, name='interpretation'),
+    #저장 결과화면
+    path('interp_save/<int:interpret_id>/', views.interpretation_save, name='interp_save'),
 ]
--- backend/interpretation/views.py
+++ backend/interpretation/views.py
@@ -7,6 +7,8 @@ from django.http import JsonResponse
 from .api import get_gemini_response
 from urllib.parse import unquote
 
+from .models import Interpret
+
 
 # Create your views here.
 ## 꿈 해몽 출력
@@ -18,15 +20,38 @@ def interpretation_view(request, content):
         dream = DreamRecord.objects.filter(context=decodedContent).first()
 
         if not decodedContent:
-            result = "꿈을 기록해 주세요."
+            interpret = None
+            interpret.result = "꿈을 기록해 주세요."
         else:
-            extra_text = ' 이 꿈 내용이 무엇을 암시하는지 해몽해 줘.'
-            full_prompt = decodedContent + extra_text
-            result = get_gemini_response(full_prompt)
+            if dream:
+                interpret = Interpret(dreamRecord=dream)
+                extra_text = ' 이 꿈 내용이 무엇을 암시하는지 해몽해 줘.'
+                full_prompt = decodedContent + extra_text
+                interpret.result = get_gemini_response(full_prompt)
+                interpret.save()
+            else:
+                interpret = Interpret()
+                interpret.result = "해당 꿈 기록이 없습니다."
+                interpret.save()
 
         return render(request, 'interpretation.html', {
-            'prompt': decodedContent,
-            'result': result,
+            'interpret': interpret,
         })
 
 
+@login_required
+def interpretation_save(request, interpret_id):
+    interpret = get_object_or_404(Interpret, id=interpret_id)
+    print("interpret:", interpret)
+    print("interpret.id:", getattr(interpret, 'id', None))
+    # if request.method == 'POST':
+    #     content = request.POST.get('content')
+    dreams = DreamRecord.objects.filter(user=request.user).order_by('-id')  # 최신순 정렬
+
+
+    return render(request, 'dreams/dream_list.html', {'dreams': dreams})
+
+# @login_required
+# def interpretation_save(request, dream_id):
+#     dream = get_object_or_404(DreamRecord, id=dream_id)
+#     return redirect
